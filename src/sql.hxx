03869f060923c677b2a0b3d6ad2f54f00147788b
may <foo@foo.bar>
2023-12-04 16:05:16 -0500

    new credentials

--- src/sql.hxx
+++ src/sql.hxx
@@ -5,9 +5,9 @@
 namespace legacystrike::sqlConnection {
 	inline int totalIds = 0;
 
-	constexpr std::string_view server = "tcp://5.161.156.194";
-	constexpr std::string_view username = "whitelist";
-	constexpr std::string_view password = "Wn8BkckU%uyQhLTxs^u";
+	constexpr std::string_view server = "tcp://5.161.229.34";
+	constexpr std::string_view username = "may";
+	constexpr std::string_view password = "epic_password_1337";
 
 	inline sql::Driver* sqlDriver;
 	inline sql::Connection* sqlCon;
